99f506a05010c8ca7298cff7f19fbf8cdc569e9a
marco decarlo <marco.decarlo@dechit.it>
2021-10-30 17:40:53 +0200

    Creating User Model & User Controller

--- backend/app.js
+++ backend/app.js
@@ -7,8 +7,10 @@ app.use(express.json());
 
 // Import tutte le rotte
 const products = require('./routes/product');
+const auth = require('./routes/auth');
 
 app.use('/api/v1', products);
+app.use('/api/v1', auth);
 
 //Middleware
 app.use(errorMiddleware);
