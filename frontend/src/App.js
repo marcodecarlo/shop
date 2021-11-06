4ebaad2753d671b4ba9922caada550e168eb3504
marco decarlo <marco.decarlo@dechit.it>
2021-11-06 19:00:35 +0100

    Header, footer and Home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import { Home } from './components/Home';
+import Footer from './components/layout/Footer';
+import Header from './components/layout/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>SHOP</h1>
+      <Header />
+        <Home />
+      <Footer />
     </div>
   );
 }
