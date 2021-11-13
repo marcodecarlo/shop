357b98b5e207cbea9a1836d357385e24175b6d1b
marco decarlo <marco.decarlo@dechit.it>
2021-11-13 14:03:44 +0100

    Adding Pagination

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -33,6 +33,7 @@ exports.getProducts = catchAsyncErrors(async (req, res, next) => {
   res.status(200).json({
     success: true,
     productsCount,
+    resPerPage,
     products,
   });
 });
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,39 +1,67 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useState } from "react";
+import Pagination from "react-js-pagination";
 import { MetaData } from "./layout/MetaData";
 
-import {useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 import { getProducts } from "../actions/productActions";
-import { useAlert } from 'react-alert';
+import { useAlert } from "react-alert";
 import Product from "./product/Product";
 import Loader from "./layout/Loader";
 
 export const Home = () => {
+  const [currentPage, setCurrentPage] = useState(1);
 
   const alert = useAlert();
   const dispatch = useDispatch();
-  const { loading, products, error, productsCount } = useSelector(state => state.products)
+  const { loading, products, error, productsCount, resPerPage } = useSelector(
+    (state) => state.products
+  );
 
   useEffect(() => {
-    if(error){
+    if (error) {
       return alert.error(error);
     }
-    dispatch(getProducts());
-  }, [dispatch,error])
+    dispatch(getProducts(currentPage));
+  }, [dispatch, alert, error, currentPage]);
+
+  function setCurrentPageNo(pageNUmber) {
+    setCurrentPage(pageNUmber);
+  }
 
   return (
     <Fragment>
-      { loading ? <Loader /> : (
+      {loading ? (
+        <Loader />
+      ) : (
         <Fragment>
-          <MetaData title={'Buy Best Products Online'} />
+          <MetaData title={"Buy Best Products Online"} />
           <h1 id="products_heading">Latest Products</h1>
 
           <section id="products" className="container mt-5">
             <div className="row">
-              { products && products.map(product =>(
-                <Product key={product._id} product={product} />
-              ))}          
+              {products &&
+                products.map((product) => (
+                  <Product key={product._id} product={product} />
+                ))}
             </div>
           </section>
+
+          {resPerPage < productsCount && (
+            <div className="d-flex justify-contente-center mt-5">
+              <Pagination
+                activePage={currentPage}
+                itemsCountPerPage={resPerPage}
+                totalItemsCount={productsCount}
+                onChange={setCurrentPageNo}
+                nextPageText={"Next"}
+                prevPageText={"Prev"}
+                firstPageText={"First"}
+                lastPageText={"Last"}
+                itemClass="page-item"
+                linkClass={"page-link"}
+              />
+            </div>
+          )}
         </Fragment>
       )}
     </Fragment>
