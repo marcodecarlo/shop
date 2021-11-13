059c34345cabc415d7615f3f2829f7395095df88
marco decarlo <marco.decarlo@dechit.it>
2021-11-13 14:57:01 +0100

    Filter by Price & Category & Ratings

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -1,20 +1,20 @@
 import React, { Fragment } from "react";
-import { Route } from 'react-router-dom';
+import { Link, Route } from "react-router-dom";
 import Search from "./Search";
 const Header = () => {
-  
-
   return (
     <Fragment>
       <nav className="navbar row">
         <div className="col-12 col-md-3">
           <div className="navbar-brand">
-            <img src="./images/logo.png" />
+            <Link to="/">
+              <img src="./images/logo.png" />
+            </Link>
           </div>
         </div>
 
         <div className="col-12 col-md-6 mt-2 mt-md-0">
-          <Route render={({ history })=> <Search history={history}/>} />
+          <Route render={({ history }) => <Search history={history} />} />
         </div>
 
         <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
