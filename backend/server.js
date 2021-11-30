46109dcf8290a7b532175ef094de652571b9a700
marco decarlo <marco.decarlo@dechit.it>
2021-11-30 20:21:25 +0100

    Adding Cloudinary for Images & Register User Component

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const app = require('./app');
 const connectDatabase = require('./config/database');
+const cloudinary = require('cloudinary');
 
 const dotenv = require('dotenv');
 
@@ -14,6 +15,13 @@ dotenv.config({ path: 'backend/config/config.env'})
 
 //Connessione database
 connectDatabase();
+
+// Setting up cloudinary configuration
+cloudinary.config({
+    cloud_name : process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const server = app.listen(process.env.PORT, () =>{
     console.log(`Il server è avviato sulla porta: ${process.env.PORT} nell'ambiente ${process.env.NODE_ENV} `)
