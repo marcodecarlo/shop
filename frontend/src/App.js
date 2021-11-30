16106c8ab72f2005735fa73c6a6cccc9df1af9f7
marco decarlo <marco.decarlo@dechit.it>
2021-11-30 20:57:08 +0100

    Load Logged In User & logout user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,23 @@
 import "./App.css";
+import { useEffect } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Home } from "./components/Home";
 import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
 import ProductDetails from "./components/product/ProductDetails";
 
-import Login from './components/user/Login';
-import Register from './components/user/Register';
+import Login from "./components/user/Login";
+import Register from "./components/user/Register";
+
+import { loadUser } from "./actions/userActions";
+import store from "./store";
 
 function App() {
+  
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
     <Router>
       <div className="App">
